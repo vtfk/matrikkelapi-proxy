f190cedcda39973d8ea42212981adb74671d7537
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2021-09-30 13:26:57 +0200

    Implementert autentisering

--- auth/authentication/apikey.js
+++ auth/authentication/apikey.js
@@ -3,6 +3,19 @@
 */
 const HeaderAPIKeyStrategy = require('passport-headerapikey').HeaderAPIKeyStrategy;
 
+function getEnvironmentAPIKeys () {
+  const APIKeys = [];
+  let currentKey = 'initial';
+  let count = 1;
+  while (currentKey !== undefined) {
+    currentKey = process.env['APIKEY' + count];
+    if (currentKey) { APIKeys.push(currentKey); } else { break; }
+    count++;
+    console.log('Counter: ' + count)
+  }
+  return APIKeys;
+}
+
 module.exports = new HeaderAPIKeyStrategy(
   {
     header: 'X-API-KEY'
@@ -10,17 +23,29 @@ module.exports = new HeaderAPIKeyStrategy(
   false,
   async (apikey, done) => {
     let isKeyFound = false;
-    console.log('Authenticate with API Key')
     // Input valdidation
-    if (!apikey) { console.log('No API key is provided'); return done(null, false); }
-    console.log('Got key: ' + apikey)
+    if (!apikey) {
+      console.log('No API key is provided');
+      return done(null, false);
+    }
+
+    // Get environment API Keys
+    const environmentkeys = getEnvironmentAPIKeys();
+    if (!environmentkeys || !Array.isArray(environmentkeys || environmentkeys.length < 1)) {
+      return done(null, false);
+    }
 
     // Check the key against environmentkey
-    if (apikey === 'asd') { isKeyFound = true; }
+    if (environmentkeys.includes(apikey)) {
+      isKeyFound = true;
+    }
 
-    // If no key is found
-    if (!isKeyFound) { console.log('❌ No matching API Key could be found'); return done(null, false); }
-    console.log('✔ API Keys is found, validated')
+    // If the key was not found
+    if (!isKeyFound) {
+      console.log('❌ No matching API Key could be found');
+      return done(null, false); 
+    }
 
+    // The key was found
     return done(null, apikey);
   })
--- test.js
+++
@@ -1,9 +0,0 @@
-const TemplateClient = require('./lib/TemplateClient/TemplateClient');
-
-const client = new TemplateClient();
-
-let matrikkelContext = client.getTemplate('matrikkelContext.xml');
-
-console.log(client.hasDefaultValues(matrikkelContext));
-matrikkelContext = client.fillDefaultValues();
-console.log(client.hasDefaultValues(matrikkelContext));
