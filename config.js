16fc40aeb978a1929035ebf813f7e5180e91db72
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2021-09-30 14:59:22 +0200

    Forsøk på å zippe bundle

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   hostname: process.env.HOSTNAME || '0.0.0.0',
   port: process.env.PORT || 8080,
-  matrikkelAPIUsername: process.env.MATRIKKELUSERNAME,
+  matrikkelAPIUsername: process.env.MATRIKKELAPIUSERNAME,
   matrikkelAPIPassword: process.env.MATRIKKELAPIPASSWORD
 }
