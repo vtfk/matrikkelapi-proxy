8ce2674c9d47eb4727db4e704c0ab1b60ceb9552
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2021-12-10 14:57:08 +0100

    Endret $ til _ på type og namespaces

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   hostname: process.env.HOSTNAME || '0.0.0.0',
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 3000,
   matrikkelAPIUsername: process.env.MATRIKKELAPIUSERNAME,
   matrikkelAPIPassword: process.env.MATRIKKELAPIPASSWORD
 }
