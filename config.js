28df9dc2293e3938d7b8158f8c0c09d6c028c409
Karl-Einar Bergstrøm <45044399+karleinarb@users.noreply.github.com>
2021-12-15 14:10:34 +0100

    Støtter nå DSF

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ module.exports = {
   hostname: process.env.HOSTNAME || '0.0.0.0',
   port: process.env.PORT || 3000,
   matrikkelAPIUsername: process.env.MATRIKKELAPIUSERNAME,
-  matrikkelAPIPassword: process.env.MATRIKKELAPIPASSWORD
+  matrikkelAPIPassword: process.env.MATRIKKELAPIPASSWORD,
+  dsfAPIEndpoint: process.env.DSFAPIENDPOINT,
+  jwtSecret: process.env.DSFSECRET
 }
